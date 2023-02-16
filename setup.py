9e52dfbc7e86d96cabb39a5015fc9975f06807f5
Merijn68 <merijn.vanmiltenburg@hotmail.com>
2023-02-16 12:53:16 +0100

    Set up repo with cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ALM Interest Rate Steering using Reinforcement Learning algorithms',
+    author='Merijn van Miltenburg',
+    license='MIT',
+)
