d0a206898ddd5a7fd7c018d26e4c9446e53f1c72
Merijn68 <merijn.vanmiltenburg@hotmail.com>
2023-08-26 14:17:27 +0200

    Train model Jupyter Notebook

--- src/tests/test_bankmodel3_a2c_train.py
+++ src/tests/test_bankmodel3_a2c_train.py
@@ -1,4 +1,5 @@
-# Lets try with vectorized environments
+""" Testing the A2C Model for the Bank Environment."""
+
 import sys
 import os
 from pathlib import Path
@@ -21,17 +22,67 @@ def linear_schedule(initial_value: float):
 
 
 def get_innermost_env(env):
+    """Get innermost non-vectorized environment."""
+
     while hasattr(env, "env"):
         env = env.env
     return env
 
 
-def main():
-    env_id = "gym_basic:bank-v3"
+def train_model(env_id, steps=50_000, log_dir="tmp/"):
+    """Train the model"""
 
     # Create log dir
-    log_dir = "tmp/"
     os.makedirs(log_dir, exist_ok=True)
+    env = gym.make(env_id, render_mode="human")
+    model = A2C(
+        "MlpPolicy",
+        env,
+        device="cpu",
+        tensorboard_log=TENSORBOARD_LOGS,
+        verbose=1,
+        n_steps=60,
+        # batch_size=n_steps * n_envs,
+        learning_rate=linear_schedule(0.001),
+        ent_coef=0.05,
+    )
+
+    model.learn(
+        total_timesteps=steps,
+        tb_log_name="ppo",
+        progress_bar=True,
+    )
+
+    env = get_innermost_env(env)
+    env.plot_rewards()
+
+    modelpath = Path(
+        MODEL_PATH,
+        "A2C_V" + "_" + str(steps) + ".zip",
+    )
+    model.save(modelpath)
+    del model  # remove to demonstrate saving and loading
+
+
+def show_model(env_id, modelpath=MODEL_PATH / "A2C_V_50000.zip") -> int:
+    """Show the model in the environment"""
+    env = gym.make(env_id, render_mode="human")
+    model = A2C.load(path=modelpath, env=env)
+    env.set_render_output(modelpath.stem)
+    obs, info = env.reset()
+    score = 0
+    terminated = False
+    truncated = False
+    while not terminated and not truncated:
+        action, _state = model.predict(obs)
+        obs, reward, terminated, truncated, info = env.step(action)
+        score = score + reward
+    env.close()
+    return score
+
+
+def random_walk(env_id):
+    """Random walk in the environment"""
     env = gym.make(env_id, render_mode="human")
     env.set_render_output("Random")
     env.reset()
@@ -49,61 +100,14 @@ def main():
     print("steps: ", steps)
     print("score: ", score)
 
-    # Create the environment vectorized
-    env = gym.make(env_id, render_mode="human")
 
-    # Create the callback: check every 60 steps
-    # callback = RewardCallback(check_freq=60, log_dir=log_dir, env=env)
-    tensorboard_logs = TENSORBOARD_LOGS
-
-    # Set your desired values
-    desired_total_steps = 50_000  # Set the total number of steps you want
-    n_steps = 60  # Set the number of steps per iteration
-    n_envs = 1  # Set the number of parallel environments
-
-    model = A2C(
-        "MlpPolicy",
-        env,
-        device="cpu",
-        tensorboard_log=tensorboard_logs,
-        verbose=1,
-        n_steps=n_steps,
-        # batch_size=n_steps * n_envs,
-        learning_rate=linear_schedule(0.001),
-        ent_coef=0.05,
-    )
-
-    model.learn(
-        total_timesteps=desired_total_steps,
-        tb_log_name="ppo",
-        progress_bar=True,
-    )
-
-    env = get_innermost_env(env)
-    env.plot_rewards()
-
-    modelpath = Path(
-        MODEL_PATH,
-        "A2C_V" + "_" + str(desired_total_steps) + ".zip",
-    )
-    model.save(modelpath)
-    del model  # remove to demonstrate saving and loading
-
-    env = gym.make(env_id, render_mode="human")
-    model = A2C.load(path=modelpath, env=env)
-    env.set_render_output(modelpath.stem)
-
-    obs, info = env.reset()
-    score = 0
-    terminated = False
-    truncated = False
-    while not terminated and not truncated:
-        action, _state = model.predict(obs)
-        obs, reward, terminated, truncated, info = env.step(action)
-        score = score + reward
-        # env.render()
-    env.close()
-
+def main():
+    env_id = "gym_basic:bank-v3"
+    # Random walk in the environment
+    random_walk(env_id)
+    # Train the model
+    train_model(env_id, steps=30_000)
+    score = show_model(env_id)
     print("score: ", score)
     print("all done... That's all folks! ")
 
